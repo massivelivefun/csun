80a00d584e6c7f2a2444b0e08730565d0f12179d
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2022-05-17 00:43:55 -0700

    Convert tabs to spaces

--- comp410/otherlangs/test.py
+++ comp410/otherlangs/test.py
@@ -1,28 +1,26 @@
 class Or:
-	def __init__(self, left, right):
-		self.left = left
-		self.right = right
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
 
-	def __str__(self):
-		return "or({}, {})". format(str(self.left),
-									str(self.right))
+    def __str__(self):
+        return "or({}, {})". format(str(self.left), str(self.right))
 
 
 class And:
-	def __init__(self, left, right):
-		self.left = left
-		self.right = right
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
 
-	def __str__(self):
-		return "and({}, {})".format(str(self.left),
-									str(self.right))
+    def __str__(self):
+        return "and({}, {})".format(str(self.left), str(self.right))
 
 
 def gen(depth):
-	yield True
-	yield False
-	if depth > 0:
-		for left in gen(depth - 1):
-			for right in gen(depth - 1):
-				yield Or(left, right)
-				yield And(left, right)
+    yield True
+    yield False
+    if depth > 0:
+        for left in gen(depth - 1):
+            for right in gen(depth - 1):
+                yield Or(left, right)
+                yield And(left, right)
